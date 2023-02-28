3da6f32440172caee84b8c461dc12be1f4b25a4e
Vasyl Puziichuk <ipzk221_pvv@student.ztu.edu.ua>
2023-02-28 10:06:24 +0200

    Add License

--- src/Function/List_2.js
+++ src/Function/List_2.js
@@ -15,64 +15,65 @@ export default function Del() {
     localStorage.setItem("messageList", JSON.stringify(state));
   }, [state]);
   function handleRemoveItem(id) {
-   setMessageList(messageList.filter((obj) => obj.id !== id));
-  //}
+    setMessageList(messageList.filter((obj) => obj.id !== id));
+    //}
 
-  const addTodo = (event) => {
-    if (event.key === "Enter") {
-      dispatch({
-        type: "add",
-        payload: todoTitle,
-      });
-      setTodoTitle("");
-    }
-  };
+    const addTodo = (event) => {
+      if (event.key === "Enter") {
+        dispatch({
+          type: "add",
+          payload: todoTitle,
+        });
+        setTodoTitle("");
+      }
+    };
 
-  return (
-    <div className={"firstl"}>
-      <h3>Лабораторна робота 4</h3>
-      <input
-        type="text"
-        value={message}
-        placeholder="Заповніть місце"
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-      />
-      <Button
-        value="Add"
-        onClick={(e) => {
-          if (message == "") {
-            dispatch({
-              type: "add",
-              payload: todoTitle,
-            });
-            message = "Пустий рядок";
-          } else {
-            setMessageList([
-              ...state,
-              {
-                id: messageList.length + 1,
-                message: message,
-                completed: false,
-              },
-            ]);
-          }
-          setMessage("");
-        }}
-      >
-        Зберегти
-      </Button>
-      <div>
-        {state.map((m) => (
-          <p className={"paragraph"} key={m.id}>
-            {m.message}{" "}
-            <Button onClick={(event) => handleRemoveItem(m.id)}>
-              Видалити
-            </Button>
-          </p>
-        ))}
+    return (
+      <div className={"firstl"}>
+        <h3>Лабораторна робота 4</h3>
+        <input
+          type="text"
+          value={message}
+          placeholder="Заповніть місце"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <Button
+          value="Add"
+          onClick={(e) => {
+            if (message == "") {
+              dispatch({
+                type: "add",
+                payload: todoTitle,
+              });
+              message = "Пустий рядок";
+            } else {
+              setMessageList([
+                ...state,
+                {
+                  id: messageList.length + 1,
+                  message: message,
+                  completed: false,
+                },
+              ]);
+            }
+            setMessage("");
+          }}
+        >
+          Зберегти
+        </Button>
+        <div>
+          {state.map((m) => (
+            <p className={"paragraph"} key={m.id}>
+              {m.message}{" "}
+              <Button onClick={(event) => handleRemoveItem(m.id)}>
+                Видалити
+              </Button>
+            </p>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
